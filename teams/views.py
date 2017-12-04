20e5c05f48b0a1fec878e8af5a26adb365047240
mauridb <mauriziocontatto@gmail.com>
2017-12-04 18:09:49 +0100

    INIT: repo soccer team

---
+++ teams/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, get_object_or_404
+
+from . import models
+
+
+def team_list(request):
+    teams = models.Team.objects.all()
+    return render(request, 'teams/team_list.html', {'teams': teams})
+
+
+def team_detail(request, pk):
+    team = get_object_or_404(models.Team, pk=pk)
+    return render(request, 'teams/team_detail.html', {'team': team})
